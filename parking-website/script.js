3e8a5fd66b1417c092684853beed96ceaf1e1d78
Jiaze <jlii0184@student.monash.edu>
2025-08-05 15:26:49 +1000

    initial map develop

--- parking-website/script.js
+++ parking-website/script.js
@@ -1,3 +1,12 @@
-function sayNo() {
-  alert("There is nothing around CBD");
-}
+// Initial Map
+const map = L.map('map').setView([-37.8136, 144.9631], 14); // Melbourne coordinate
+
+// Load Map（OpenStreetMap）
+L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+  attribution: '© OpenStreetMap contributors'
+}).addTo(map);
+
+// example, add a parking spot marker
+L.marker([-37.814, 144.963]).addTo(map)
+  .bindPopup('Parking Spot 1')
+  .openPopup();
